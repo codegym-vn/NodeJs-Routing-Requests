474f117a9134ad87fa05f64710fd7aaad00a33c3
vukieuanh <vukieuanh.hnue@gmail.com>
2022-04-18 16:30:02 +0700

    node js routing request

--- app.js
+++ app.js
@@ -3,35 +3,24 @@ var http = require('http');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
 
-//
 var server = http.createServer(function (req, res) {
     //get url and parse
     var parseUrl = url.parse(req.url, true);
-    //
     // //get the path
     var path = parseUrl.pathname;
     var trimPath = path.replace(/^\/+|\/+$/g, '');
-    //method
-    var method = req.method.toLowerCase();
+    // console.log(trimPath);
+    // res.end();
 
-    var queryStringObject = parseUrl.query;
-
-    var decoder = new StringDecoder('utf-8');
-    var buffer = '';
     req.on('data', function (data) {
-        buffer += decoder.write(data);
     });
     req.on('end', function (end) {
-        buffer += decoder.end();
-
         var chosenHandler = (typeof (router[trimPath]) !== 'undefined') ? router[trimPath] : handlers.notFound;
-
         var data=
             {
                 "trimPath": trimPath
             }
         ;
-
         chosenHandler(data, function (statusCode, payload) {
             statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
             payload = typeof (payload) == 'object' ? payload : {};
@@ -47,7 +36,7 @@ var server = http.createServer(function (req, res) {
 })
 //server start
 server.listen(3000, function () {
-    console.log("dang chay r nhe");
+    console.log("server running at localhost:3000 ");
 })
 
 
@@ -58,12 +47,19 @@ handlers.sample = function (data, callback) {
 // call back
     callback(406, {'name': 'sample handle'})
 };
-// //not found sample
+//not found sample
 handlers.notFound = function (data, callback) {
     callback(404);
 };
 
+//home
+handlers.home = function (data, callback) {
+// call back
+    callback(200, 'home page');
+};
+
 //definer the request router
 var router = {
-    'sample': handlers.sample
+    'sample': handlers.sample,
+    'home': handlers.home,
 }
